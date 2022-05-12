667716658e5045d957ffa9796b225d55238f0117
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:34 -0300

    chore: re index finalseg

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."finalseg-master".dir   = "master";
+  inputs."finalseg-master".owner = "nim-nix-pkgs";
+  inputs."finalseg-master".ref   = "master";
+  inputs."finalseg-master".repo  = "finalseg";
+  inputs."finalseg-master".type  = "github";
+  inputs."finalseg-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."finalseg-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
